93adb2ce63b24d210fa152497363e1c8387445a3
Raphael Da Silva <raphds@yahoo.fr>
2023-07-06 07:43:52 +0200

    Debut responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
+import { slide as Menu } from "react-burger-menu";
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      <Menu right>
+        <NavLink to="/">
+          <li>Accueil</li>
+        </NavLink>
+        <NavLink to="/videos">
+          <li>Vidéos</li>
+        </NavLink>
+        <NavLink to="/photos">
+          <li>Photos</li>
+        </NavLink>
+        <NavLink to="/contact">
+          <li>Contact</li>
+        </NavLink>
+      </Menu>
       <div className="logonavbar">
         <Logo />
       </div>
