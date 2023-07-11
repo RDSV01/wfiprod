6251cc4d34ed1b9c1d6ddb03032a40e382ec138e
Raphael Da Silva <raphds@yahoo.fr>
2023-07-11 22:02:58 +0200

    Version fonctionelle, barre de scroll a modifier et annimation page video a modofier

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./assets/css/App.css";
+import Home from "./pages/Home";
+import Videos from "./pages/Videos";
+import Photos from "./pages/Photos";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/photos" element={<Photos />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
