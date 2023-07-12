bdfe77454f205fe2d243919f74fa37adbde4c77b
Raphael Da Silva <raphds@yahoo.fr>
2023-07-12 23:05:53 +0200

    Nouvelle utilisation de l'api youtube avec un module

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,14 +1,19 @@
 import GalleryVideo from "../components/GalleryVideo";
 import Navbar from "../components/Navbar";
-import { Bounce } from "react-awesome-reveal";
+import { Zoom } from "react-awesome-reveal";
+import YouTubePlaylist from "@codesweetly/react-youtube-playlist";
 
 const videos = () => {
   return (
     <div>
       <Navbar />
-      <Bounce>
-        <GalleryVideo />
-      </Bounce>
+      <Zoom>
+        <YouTubePlaylist
+          apiKey="AIzaSyAeIaqpDrH5tYTftRSISnJjEtS3sD0K-mA"
+          playlistId="PL_Yk0jY7rkyBYBb3ghH82M7ku6sMFoz25"
+          uniqueName="apivideos"
+        />
+      </Zoom>
     </div>
   );
 };
