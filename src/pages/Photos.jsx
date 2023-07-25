bbb0f668c8f8ce30701bc1aae74a95d68e19b4c9
Raphael Da Silva <raphds@yahoo.fr>
2023-07-25 22:42:31 +0200

    Debut carousel photo

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import CarouselPhotos from "../components/CarouselPhotos";
 
 const Photos = () => {
   return (
     <div>
       <Navbar />
+      <CarouselPhotos />
     </div>
   );
 };
